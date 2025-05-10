6863c24647461da380bb0e689c79861718bbcfd0
MLenaBleile <mbleile@smu.edu>
2025-05-10 00:14:27 -0400

    remove js analytics

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { getFirestore, collection, addDoc, getDocs, orderBy, query } from 'fireb
 import { onSnapshot } from 'firebase/firestore';
 
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,7 +21,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 
 // Initialize Firebase
 
